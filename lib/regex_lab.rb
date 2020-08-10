a3c556705fbfc0dea90ddccb2be197f919b664f8
Trevis McGill <trevismcgill25@gmail.com>
2020-08-10 03:34:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,5 +19,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/(^\d{10}$)/) != nil
+puts phone.match(/(^\d{10}$)/) != nil
+binding.pry
 end

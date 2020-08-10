8f28cf704d1495c67a35d83fd3b08798ffbfab6f
Trevis McGill <trevismcgill25@gmail.com>
2020-08-10 02:43:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- text.match(/\A[A-Z]\w*\W\b/) != nil
+ text.match(/\A[A-Z]\w*\S\b/) != nil
 end
 
 def valid_phone_number?(phone)
